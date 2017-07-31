5a237049ed7cc5e98ee0f87ed407bf6f8302f76b
John Coene <jcoenep@gmail.com>
2017-07-31 22:56:39 +0800

    new funs

--- R/utils.R
+++ R/utils.R
@@ -26,3 +26,16 @@ b_stack <- function(serie, name){
   if(is.null(name)) name <- deparse(substitute(serie, parent.frame()))
   name
 }
+
+pie_dat <- function(serie){
+  data <- get("data", envir = data_env)
+  x <- get("x", envir = data_env)
+  x <- eval(substitute(x), data)
+
+  serie <- eval(substitute(serie, parent.frame()), data)
+
+  sapply(1:length(serie), function(i, x, serie){
+    list(c(x[i], serie[i]))
+  }, x, serie)
+
+}
